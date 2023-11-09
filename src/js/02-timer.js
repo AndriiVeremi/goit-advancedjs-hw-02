f2f190c99d73169d04ce5b0226922e8ca1d68407
Andrii Veremii <dashuk10@gmail.com>
2023-11-09 17:00:13 +0100

    add

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,8 +1,16 @@
-
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+};
+
+refs.startBtn.disabled = true;
+refs.startBtn.addEventListener('click', onStartEvent);
+  
+let selectedNewDates = null;
 
 const options = {
   enableTime: true,
@@ -10,38 +18,46 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+      console.log(selectedDates[0]);
+      
+
+    if (selectedDates[0] <= new Date()) {
+      iziToast.show({
+        position: 'center',
+        title: 'Sorry !',
+        message: 'Please choose a date in the future',
+      });
+    } else {
+        selectedNewDates = selectedDates[0].getTime();
+        refs.startBtn.disabled = false;
+      }
   },
 };
 
 flatpickr('input#datetime-picker', options);
 
 
-
-
-
-
-
-
-
+function onStartEvent() {
+console.log(selectedNewDates);
+}
 
 function convertMs(ms) {
-// Number of milliseconds per unit of time
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-// Remaining days
-const days = Math.floor(ms / day);
-// Remaining hours
-const hours = Math.floor((ms % day) / hour);
-// Remaining minutes
-const minutes = Math.floor(((ms % day) % hour) / minute);
-// Remaining seconds
-const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-return { days, hours, minutes, seconds };
+  return { days, hours, minutes, seconds };
 }
 
 // console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
